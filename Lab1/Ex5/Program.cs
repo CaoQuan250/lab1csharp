8ca5260b5a372de8799706e4a974b26e9e2bdd33
CaoQuan250 <97829072+CaoQuan250@users.noreply.github.com>
2022-08-01 08:38:20 +0700

    Add files via upload

---
+++ Lab1/Ex5/Program.cs
@@ -0,0 +1,21 @@
+﻿class Ex5
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input factorial: ");
+        int n = int.Parse(Console.ReadLine());
+        if(n < 0)
+        {
+            Console.WriteLine("Factorial of a negative number doesn't exist.");
+        }
+        else
+        {
+            long l = 1;
+            for(int i = 1; i < n; i++)
+            {
+                l = l * i;
+            }
+            Console.WriteLine("Factorial of " + n + " = " + l);
+        }
+    }
+}
